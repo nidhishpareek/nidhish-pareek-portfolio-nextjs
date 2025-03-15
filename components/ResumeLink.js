9e8fd23d59c37417cc5da249f59dc7c11cc5fd1d
Nidhish Pareek <122966007+nidhishcimet@users.noreply.github.com>
2025-03-15 20:32:55 +0530

    update resume link

--- components/ResumeLink.js
+++ components/ResumeLink.js
@@ -1,2 +1,2 @@
-export const resume_link=
-  "https://drive.google.com/uc?id=1O1DkpU-3zp3TLxMwpDT-dRYagOfHOEAR&export=download?usp=share_link";
+export const resume_link =
+  "https://drive.google.com/file/d/1aIMzSj1h6wJ30hdIdFUqk-2hdieD5eax/view?usp=sharing";
